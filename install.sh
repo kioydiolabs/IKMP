96a50aa72604dba14be6b49abb642eca0aab73ba
Stratos Thivaios <admin@kioydio.org>
2024-05-29 16:54:57 +0300

    added -q to wget in install.sh

--- install.sh
+++ install.sh
@@ -1,13 +1,13 @@
 cd /etc
 mkdir ikmp
 cd /etc/ikmp
-wget https://raw.githubusercontent.com/kioydiolabs/IKMP/main/ikmp_web.py
-wget https://raw.githubusercontent.com/kioydiolabs/IKMP/main/ikmp.py
-wget https://raw.githubusercontent.com/kioydiolabs/IKMP/main/config.json
-wget https://raw.githubusercontent.com/kioydiolabs/IKMP/main/uptime.json
-wget https://raw.githubusercontent.com/kioydiolabs/IKMP/main/requirements.txt
-wget https://raw.githubusercontent.com/kioydiolabs/IKMP/main/ikmp_web.service --directory-prefix=/etc/systemd/system
-wget https://raw.githubusercontent.com/kioydiolabs/IKMP/main/ikmp.service --directory-prefix=/etc/systemd/system
+wget https://raw.githubusercontent.com/kioydiolabs/IKMP/main/ikmp_web.py -q
+wget https://raw.githubusercontent.com/kioydiolabs/IKMP/main/ikmp.py -q
+wget https://raw.githubusercontent.com/kioydiolabs/IKMP/main/config.json -q
+wget https://raw.githubusercontent.com/kioydiolabs/IKMP/main/uptime.json -q
+wget https://raw.githubusercontent.com/kioydiolabs/IKMP/main/requirements.txt -q
+wget https://raw.githubusercontent.com/kioydiolabs/IKMP/main/ikmp_web.service -q --directory-prefix=/etc/systemd/system
+wget https://raw.githubusercontent.com/kioydiolabs/IKMP/main/ikmp.service -q --directory-prefix=/etc/systemd/system
 systemctl reload-daemon
 systemctl enable ikmp ikmp_web
 apt install pip3 -y

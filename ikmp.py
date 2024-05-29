3053ba9fb2e109e671f2a21aab210ef727f77ce2
Stratos Thivaios <admin@kioydio.org>
2024-05-29 16:25:46 +0300

    Commit all files v.1

---
+++ ikmp.py
@@ -0,0 +1,47 @@
+from icmplib import ping
+import json
+import os
+import time
+
+
+with open('config.json') as fd:
+    json_data = json.load(fd)
+
+def host_up(hostname: str):
+    host = ping(hostname, count=5, interval=0.2)
+    return host.packets_senXt == host.packets_received
+
+
+with open('uptime.json', 'r') as f:
+    uptimeFile = json.load(f)
+
+interval = json_data['interval']
+debug = int(json_data['debug'])
+
+def uptime():
+    for i in json_data['hosts']:
+        if debug == 1 or debug == 2:
+            print(json_data['hosts'][i]['address'])
+        host = json_data['hosts'][i]['address']
+
+        try:
+            if host_up(host):
+                if debug == 1 or debug == 2:
+                    print("UP")
+                uptimeFile['hosts'][i] = (f"UP")
+        except Exception as e:
+            if debug == 1:
+                print("DOWN")
+            if debug == 2:
+                print("DOWN - Error :",e)
+            uptimeFile['hosts'][i] = (f"DOWN")
+
+        os.remove('uptime.json')
+        with open('uptime.json', 'w') as f:
+            json.dump(uptimeFile, f, indent=4)
+
+while True:
+    uptime();
+    if debug == 1 or debug == 2:
+        print(f"Waiting for {interval} seconds")
+    time.sleep(int(interval))
---
+++ ikmp_web.py
@@ -0,0 +1,29 @@
+from flask import Flask, request, Response, render_template
+import json
+
+with open('config.json', 'r') as f:
+        config = json.load(f)
+color_up = config['colors']['up']
+color_down = config['colors']['down']
+
+app = Flask(__name__, template_folder="./templates/")
+
+
+@app.route("/")
+def index():
+        data = ""
+        with open('config.json', 'r') as f:
+                config = json.load(f)
+        with open('uptime.json', 'r') as f:
+                uptimeFile = json.load(f)
+        for i in config['hosts']:
+                if uptimeFile['hosts'][i] == "UP":
+                        color = color_up
+                elif uptimeFile['hosts'][i] == "DOWN":
+                        color = color_down
+                data += (f"<div style='display: flex; flex-direction: row; align-items: center; justify-items: center;'><a style='color: white;' href='{config['hosts'][i]['webpage_link']}'>{config['hosts'][i]['webpage_name']}</a><p style='color: {color};'>  - {uptimeFile['hosts'][i]}</p></div>")
+        return render_template('web.template.html',data = data)
+
+
+app.run(host='0.0.0.0', port=80,
+        debug=True)
